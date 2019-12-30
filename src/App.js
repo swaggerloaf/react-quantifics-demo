6a6994bc711bcc9bda7258a6391e7f41298e3e03
swaggerloaf <swaggerloaf@gmail.com>
2019-12-29 19:32:01 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-primary">
+        <h2>what a beautiful day in the neighborhood!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
